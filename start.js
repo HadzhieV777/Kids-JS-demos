0ad9de387f880680ed7a98c3bc04ddcdaf215f2c
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-02-11 06:15:39 +0530

    added rockPaperScissor
    just did it for fun XD

--- start.js
+++ start.js
@@ -21,4 +21,82 @@ function stringManipulator(name, age, town) {
             I am ${age} years old and I am living in ${town}`
 }
 
-console.log(stringManipulator('Pesho', 16, 'Svilengrad'))
+// console.log(stringManipulator('Pesho', 16, 'Svilengrad'))
+
+function rockPaperScissor(){
+   const bot_choice = Math.floor(Math.random() * 3);
+   const user_choice = prompt("Type your choice in numbers(Any 1 of them): \n 1.Rock \n 2.Paper \n 3.Scissor");
+   var choices = {
+      bot: "",
+      user: ""
+   };
+
+   function restart(){
+      const res = prompt('Restart? [yes/no]');
+      if(res == 'yes'){
+         rockPaperScissor();
+      } else {
+         
+      };
+   }
+   
+   if(user_choice == 1 && bot_choice == 0){
+      choices[0] = "Rock";
+      choices[1] = "Rock";
+      alert(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      const res = prompt('Restart?');
+      restart();
+   } else if(user_choice == 1 && bot_choice == 1){
+      choices[0] = "Paper";
+      choices[1] = "Rock";
+      alert(`Bot won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 1 && bot_choice == 2){
+      choices[0] = "Scissor";
+      choices[1] = "Rock";
+      alert(`You Won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 2 && bot_choice == 0){
+      choices[0] = "Rock";
+      choices[1] = "Paper";
+      alert(`You won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 2 && bot_choice == 1){
+      choices[0] = "Paper";
+      choices[1] = "Paper";
+      alert(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 2 && bot_choice == 2){
+      choices[0] = "Scissor";
+      choices[1] = "Paper";
+      alert(`Bot won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 3 && bot_choice == 0){
+      choices[0] = "Rock";
+      choices[1] = "Scissor";
+      alert(`Bot Won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 3 && bot_choice == 1){
+      choices[0] = "Paper";
+      choices[1] = "Scissor";
+      alert(`You Won! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   } else if(user_choice == 3 && bot_choice == 2){
+      choices[0] = "Scissor";
+      choices[1] = "Scissor";
+      alert(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      console.log(`Its a tie! \n [Chosen options: \n You: ${choices[1]}, \n Bot: ${choices[0]}]`);
+      restart();
+   }
+};
+
+
+rockPaperScissor();
